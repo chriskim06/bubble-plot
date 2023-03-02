438f0d9bacb0b9dfbfeeb08e3f7caf65b0ee4e85
Chris Kim <chriskim06@gmail.com>
2023-03-02 08:20:38 -0800

    asdf

--- update.go
+++ update.go
@@ -22,7 +22,7 @@ func (m *Model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
 		m.SetSize(msg)
 	case GraphUpdateMsg:
 		m.data = msg.Data
-		m.canvas.HorizontalLabels = msg.Labels
+		//         m.canvas.HorizontalLabels = msg.Labels
 	}
 	return m, nil
 }
@@ -43,8 +43,8 @@ func (m *Model) SetSize(msg tea.WindowSizeMsg) {
 		lineColors = append(lineColors, drawille.Color(c))
 	}
 	canvas.LineColors = lineColors
-	if len(m.horizontalLabels) > 0 {
-		canvas.HorizontalLabels = m.horizontalLabels
-	}
+	//     if len(m.horizontalLabels) > 0 {
+	//         canvas.HorizontalLabels = m.horizontalLabels
+	//     }
 	m.canvas = &canvas
 }
