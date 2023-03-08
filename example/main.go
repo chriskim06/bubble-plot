c0a887c1d4689108fcb5d258ac74f63fc0ccf3c6
Chris Kim <chriskim06@gmail.com>
2023-03-08 09:33:38 -0800

    add example

--- example/main.go
+++ example/main.go
@@ -2,25 +2,27 @@ package main
 
 import (
 	"fmt"
+	"math"
 	"os"
+	"time"
 
 	tea "github.com/charmbracelet/bubbletea"
-	"github.com/chriskim06/bubble-plot/plot"
+	plot "github.com/chriskim06/bubble-plot"
 )
 
 var x = -1
 
 type model struct {
 	data [][]float64
-	p plot.Model
+	p    *plot.Model
 }
 
-func newmodel() model {
+func newmodel() *model {
 	pl := plot.New()
-	pl.MaxDataPoints = 50
-	return model{
+	pl.MaxDataPoints = 40
+	return &model{
 		data: [][]float64{{}, {}},
-		p: plot.New(),
+		p:    pl,
 	}
 }
 
@@ -28,45 +30,54 @@ func (m model) Init() tea.Cmd {
 	return nil
 }
 
-func (m *model) Update(msg tea.Msg) (model, tea.Cmd) {
+func (m *model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
 	switch msg := msg.(type) {
 	case tea.WindowSizeMsg:
 		m.p.Update(tea.WindowSizeMsg{
-			Width: 120,
-			Height: 80,
+			Width:  120,
+			Height: 40,
 		})
+		return m, m.tick()
+	case tea.KeyMsg:
+		if msg.String() == "q" {
+			return m, tea.Quit
+		}
 	case tickmsg:
 		// update data
-		if len(m.data[0]) == 50 {
+		if len(m.data[0]) == 40 {
 			m.data[0] = m.data[0][1:]
 			m.data[1] = m.data[1][1:]
 		}
-		m.data[0] = append(m.data[0], 40)
+		m.data[0] = append(m.data[0], 3)
 		m.data[1] = append(m.data[1], sindata())
-		return *m, m.tick()
+		m.p.Update(plot.GraphUpdateMsg{
+			Data: m.data,
+		})
+		return m, m.tick()
 	}
-	return *m, nil
+	return m, nil
 }
 
 func (m model) View() string {
-	return m.p.Plot(m.data)
+	return m.p.View()
 }
 
 type tickmsg struct{}
 
-func (m mode) tick() tea.Cmd {
-	return tea.Tick(2*time.Second, func(t time.Time) tea.Msg {
+func (m model) tick() tea.Cmd {
+	return tea.Tick(time.Second, func(t time.Time) tea.Msg {
 		return tickmsg{}
 	})
 }
 
 func sindata() float64 {
 	x++
-	return math.Sin((math.Pi/180) * float64(10*x)) + 10
+	return 2 * math.Sin((math.Pi/9)*float64(x))
 }
 
 func main() {
-	if _, err := tea.NewProgram(newmodel()).Run(); err != nil {
+	m := newmodel()
+	if _, err := tea.NewProgram(m).Run(); err != nil {
 		fmt.Println(err)
 		os.Exit(1)
 	}
